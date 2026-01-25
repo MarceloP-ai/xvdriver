49eb2fcb8de2515deb25553567a0735622bd02b8
Marcelo <seu-email@exemplo.com>
2026-01-25 19:55:50 -0300

    V11: ICD Native Provider Mode

--- src/vk_wrapper.cpp
+++ src/vk_wrapper.cpp
@@ -1,22 +1,21 @@
 #include <vulkan/vulkan.h>
 #include <android/log.h>
+#include <string.h>
 
 #define LOG_TAG "XVD_DEBUG"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 extern "C" {
-    // Forçamos o log assim que a biblioteca é carregada na memória do Eden
-    void __attribute__((constructor)) init() {
-        LOGI("XVD_DEBUG: DRIVER LOADED INTO MEMORY!");
-    }
-
-    // Interceptador de funções do sistema
-    VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetInstanceProcAddr(VkInstance instance, const char* pName) {
-        LOGI("XVD_DEBUG: Eden requested function: %s", pName);
-        return nullptr; // Deixa o sistema seguir após logarmos
-    }
-
-    VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetDeviceProcAddr(VkDevice device, const char* pName) {
+    // Essa função é o que o Android busca primeiro ao carregar um driver Vulkan
+    VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_icdGetInstanceProcAddr(VkInstance instance, const char* pName) {
+        LOGI("XVD_DEBUG: ICD Hook success for %s", pName);
         return nullptr;
+    }
+
+    // Função de fallback para o carregador
+    VKAPI_ATTR VkResult VKAPI_CALL vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t* pSupportedVersion) {
+        LOGI("XVD_DEBUG: ICD version negotiated");
+        *pSupportedVersion = 2;
+        return VK_SUCCESS;
     }
 }
