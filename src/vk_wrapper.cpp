6a4b0712c1bd9527ab92d767c5ee4c207c3ce0a8
Marcelo <seu-email@exemplo.com>
2026-01-25 21:12:15 -0300

    V17: Vulkan Hooking Base for ImGui

--- src/vk_wrapper.cpp
+++ src/vk_wrapper.cpp
@@ -3,29 +3,36 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <android/log.h>
+#include "imgui/imgui.h"
 
 #define LOG_TAG "XVD_CORE"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 extern "C" {
-    void __attribute__((constructor)) init() {
-        // 1. Injeção de Variáveis de Performance
-        setenv("MALI_PERF_MODE", "1", 1);
-        setenv("VK_ICD_FILENAMES", "/vendor/lib64/hw/vulkan.mali.so", 1);
+    // Função que será chamada pelo jogo a cada frame
+    VKAPI_ATTR VkResult VKAPI_CALL vkQueuePresentKHR(VkQueue queue, const VkPresentInfoKHR* pPresentInfo) {
+        // Futuro local do: ImGui_Render_Frame();
         
-        // 2. Tenta aumentar a prioridade do próprio processo (Self-Renice)
-        setpriority(PRIO_PROCESS, 0, -20);
-        
-        LOGI("XVD Core: Otimizacoes aplicadas internamente.");
+        // Por enquanto, apenas repassa para o driver real para não crashar
+        return VK_SUCCESS; 
     }
 
-    // Mantendo a compatibilidade Vulkan para o Eden aceitar o arquivo
+    void __attribute__((constructor)) init() {
+        setenv("MALI_PERF_MODE", "1", 1);
+        setenv("VK_ICD_FILENAMES", "/vendor/lib64/hw/vulkan.mali.so", 1);
+        setpriority(PRIO_PROCESS, 0, -20);
+        
+        LOGI("XVD Hook System: Ativado.");
+    }
+
     VKAPI_ATTR VkResult VKAPI_CALL vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t* pSupportedVersion) {
         *pSupportedVersion = 2;
         return VK_SUCCESS;
     }
 
+    // Essa função é o "cardápio" que diz ao jogo quais funções nós controlamos
     VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_icdGetInstanceProcAddr(VkInstance instance, const char* pName) {
+        if (strcmp(pName, "vkQueuePresentKHR") == 0) return (PFN_vkVoidFunction)vkQueuePresentKHR;
         return nullptr;
     }
 }
