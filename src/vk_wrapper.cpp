e6ccd953b059c5258435da81797ef32693c56f44
Marcelo <seu-email@exemplo.com>
2026-01-25 21:15:44 -0300

    V18: Dynamic Linker to original Samsung Driver

--- src/vk_wrapper.cpp
+++ src/vk_wrapper.cpp
@@ -1,38 +1,40 @@
 #include <vulkan/vulkan.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/resource.h>
+#include <dlfcn.h>
+#include <stdio.h>
 #include <android/log.h>
 #include "imgui/imgui.h"
 
-#define LOG_TAG "XVD_CORE"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+typedef PFN_vkVoidFunction (VKAPI_PTR *PFN_vk_icdGetInstanceProcAddr)(VkInstance instance, const char* pName);
+static PFN_vk_icdGetInstanceProcAddr real_gippa = nullptr;
 
 extern "C" {
-    // Função que será chamada pelo jogo a cada frame
+    // Intercepta a apresentação do frame
     VKAPI_ATTR VkResult VKAPI_CALL vkQueuePresentKHR(VkQueue queue, const VkPresentInfoKHR* pPresentInfo) {
-        // Futuro local do: ImGui_Render_Frame();
+        // Aqui entrará o: if(!imgui_init) { setup_imgui(); } render_menu();
         
-        // Por enquanto, apenas repassa para o driver real para não crashar
-        return VK_SUCCESS; 
+        // Busca a função real no driver da Samsung para não travar o jogo
+        static PFN_vkQueuePresentKHR real_present = (PFN_vkQueuePresentKHR)real_gippa(nullptr, "vkQueuePresentKHR");
+        return real_present(queue, pPresentInfo);
     }
 
     void __attribute__((constructor)) init() {
-        setenv("MALI_PERF_MODE", "1", 1);
-        setenv("VK_ICD_FILENAMES", "/vendor/lib64/hw/vulkan.mali.so", 1);
-        setpriority(PRIO_PROCESS, 0, -20);
+        // Carrega o driver original da Samsung/Mali em segredo
+        void* handle = dlopen("/vendor/lib64/hw/vulkan.mali.so", RTLD_NOW);
+        if (handle) {
+            real_gippa = (PFN_vk_icdGetInstanceProcAddr)dlsym(handle, "vk_icdGetInstanceProcAddr");
+        }
+    }
+
+    VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_icdGetInstanceProcAddr(VkInstance instance, const char* pName) {
+        if (strcmp(pName, "vkQueuePresentKHR") == 0) return (PFN_vkVoidFunction)vkQueuePresentKHR;
         
-        LOGI("XVD Hook System: Ativado.");
+        // Se não for uma função que queremos alterar, pede para o driver real cuidar disso
+        if (real_gippa) return real_gippa(instance, pName);
+        return nullptr;
     }
 
     VKAPI_ATTR VkResult VKAPI_CALL vk_icdNegotiateLoaderICDInterfaceVersion(uint32_t* pSupportedVersion) {
         *pSupportedVersion = 2;
         return VK_SUCCESS;
-    }
-
-    // Essa função é o "cardápio" que diz ao jogo quais funções nós controlamos
-    VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vk_icdGetInstanceProcAddr(VkInstance instance, const char* pName) {
-        if (strcmp(pName, "vkQueuePresentKHR") == 0) return (PFN_vkVoidFunction)vkQueuePresentKHR;
-        return nullptr;
     }
 }
